9e097c4808d5b1b0d0800946b2659bb7e4de4366
Cuda-Chen <clh960524@gmail.com>
2020-02-17 22:34:21 +0800

    create workable version

--- main.py
+++ main.py
@@ -1,56 +1,20 @@
 import numpy as np
-from numpy.linalg import lstsq
-
-ramp = lambda u: np.maximum( u, 0 )
-step = lambda u: ( u > 0 ).astype(float)
-
-def SegmentedLinearReg( X, Y, breakpoints ):
-    nIterationMax = 10
-
-    breakpoints = np.sort( np.array(breakpoints) )
-
-    dt = np.min( np.diff(X) )
-    ones = np.ones_like(X)
-
-    for i in range( nIterationMax ):
-        # Linear regression:  solve A*p = Y
-        Rk = [ramp( X - xk ) for xk in breakpoints ]
-        Sk = [step( X - xk ) for xk in breakpoints ]
-        A = np.array([ ones, X ] + Rk + Sk )
-        p =  lstsq(A.transpose(), Y, rcond=None)[0] 
-
-        # Parameters identification:
-        a, b = p[0:2]
-        ck = p[ 2:2+len(breakpoints) ]
-        dk = p[ 2+len(breakpoints): ]
-
-        # Estimation of the next break-points:
-        newBreakpoints = breakpoints - dk/ck 
-
-        # Stop condition
-        if np.max(np.abs(newBreakpoints - breakpoints)) < dt/5:
-            break
-
-        breakpoints = newBreakpoints
-    else:
-        print( 'maximum iteration reached' )
-
-    # Compute the final segmented fit:
-    Xsolution = np.insert( np.append( breakpoints, max(X) ), 0, min(X) )
-    ones =  np.ones_like(Xsolution) 
-    Rk = [ c*ramp( Xsolution - x0 ) for x0, c in zip(breakpoints, ck) ]
-
-    Ysolution = a*ones + b*Xsolution + np.sum( Rk, axis=0 )
-
-    return Xsolution, Ysolution
-
 import matplotlib.pyplot as plt
+import pwlf
 
-X = np.linspace( 0, 10, 27 )
-Y = 0.2*X  - 0.3* ramp(X-2) + 0.3*ramp(X-6) + 0.05*np.random.randn(len(X))
-plt.plot( X, Y, 'ok' );
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], dtype=float)
+y = np.array([0, 12.05, 0, 12.66, 40.0, 
+     0, 0, 0, 0, 5.1,
+     0, 0, 3.51, 0, 0])
 
-initialBreakpoints = [1, 7]
-plt.plot( *SegmentedLinearReg( X, Y, initialBreakpoints ), '-r' );
-plt.xlabel('X'); plt.ylabel('Y');
+my_pwlf = pwlf.PiecewiseLinFit(x, y)
+breaks = my_pwlf.fit(13)
+print(breaks)
+
+x_hat = np.linspace(x.min(), x.max(), 100)
+y_hat = my_pwlf.predict(x_hat)
+
+plt.figure()
+plt.plot(x, y, 'o')
+plt.plot(x_hat, y_hat, '-')
 plt.show()
